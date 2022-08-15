0692e32cc2c4715d564030904a47d8be664fec32
bowei liu <liubowei@gmail.com>
2022-08-15 21:36:26 +0000

    works but a double import

--- www/bootstrap.js
+++ www/bootstrap.js
@@ -2,4 +2,9 @@
 // asynchronously. This `bootstrap.js` file does the single async import, so
 // that no one else needs to worry about it again.
 import("./index.js")
+  .then(() => console.log('bootstrapjs finished import.'))
   .catch(e => console.error("Error importing `index.js`:", e));
+
+var x = 3;
+console.log('bootstrap.js was here' + x)
+x+= 1;
--- www/index.js
+++ www/index.js
@@ -1,6 +1,13 @@
 // import * as wasm from "hello-wasm-pack";
 import * as wasm from "wasm-game-of-life";
 
-wasm.greet("you");
+var x = 1;
 
-wasm.console("hello");
+// wasm.greet("you" + x);
+console.log('index' + x);
+x += 1;
+
+// wasm.greet("hello" + x);
+x += 1;
+
+
