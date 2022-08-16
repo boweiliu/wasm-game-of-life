ca017df30c1d86105dfc885b39e82e949a253e62
bowei liu <liubowei@gmail.com>
2022-08-16 16:08:36 +0000

    yay things working.

--- www/index.js
+++ www/index.js
@@ -26,6 +26,10 @@ const ALIVE_COLOR = "#000000";
 // 
 
 
+const universe = Universe.new();
+const width = universe.width();
+const height = universe.height();
+
 const canvas = document.getElementById("game-of-life-canvas");
 canvas.height = (CELL_SIZE + 1) * height + 1;
 canvas.width = (CELL_SIZE + 1) * width + 1;
